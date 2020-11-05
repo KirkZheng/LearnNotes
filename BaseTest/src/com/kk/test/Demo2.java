94dcb110fa0ca48e59cc453b5ea3a495cac0869f
kirkzheng <1757225882@qq.com>
2020-11-05 15:51:36 +0800

    首次上传

---
+++ BaseTest/src/com/kk/test/Demo2.java
@@ -0,0 +1,39 @@
+package com.kk.test;
+import java.util.Scanner;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("请输入一个1-7：");
+		int number = sc.nextInt();	
+		switch(number) {
+		case 1:
+			System.out.println("星期以一");
+			break;
+		case 2:
+			System.out.println("星期二");
+			break;
+		case 3:
+			System.out.println("星期三");
+			break;
+		case 4:
+			System.out.println("星期四");
+			break;
+		case 5:
+			System.out.println("星期五");
+			break;
+		case 6:
+			System.out.println("星期六");
+			break;
+		case 7:
+			System.out.println("星期日");
+			break;
+		default: 
+			System.out.println("输入有误，程序终止！");
+		}
+		
+	}
+
+}
